293452797c4781f970fb9592e113ac61f47b684b
JhonatanGAlves <jhonatangalves96@gmail.com>
2024-05-19 12:16:02 -0300

    feat: ✨ creating GetUserByEmailRepository class

---
+++ src/repositories/postgres/get-user-by-email.js
@@ -0,0 +1,12 @@
+import { PostgresHelper } from '../../db/postgres/helper.js'
+
+export class PostgresGetUserByEmailRepository {
+    async execute(userEmail) {
+        const userSelectedByEmail = await PostgresHelper.query(
+            'SELECT * FROM users WHERE email = $1',
+            [userEmail],
+        )
+
+        return userSelectedByEmail[0]
+    }
+}
