da7a642ee4412a6d07b9daa9da5354b5b73ec4d3
JhonatanGAlves <jhonatangalves96@gmail.com>
2024-05-18 22:35:34 -0300

    feat: ✨ creating CreateUserController class and adding validations

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import 'dotenv/config.js'
 import express from 'express'
+import { CreateUserController } from './src/controllers/create-user.js'
 
 const app = express()
 
 app.use(express.json())
 
-app.listen(process.env.PORT, async () => console.log('Listening on port 8080'))
+app.post('/api/users', async (request, response) => {
+    const createUserController = new CreateUserController()
+    const { statusCode, body } = await createUserController.execute(request)
+
+    return response.status(statusCode).send(body)
+})
+
+app.listen(process.env.PORT, async () =>
+    console.log(`Listening on port ${process.env.PORT}`),
+)
---
+++ src/controllers/create-user.js
@@ -0,0 +1,46 @@
+import validator from 'validator'
+
+import { CreateUserUseCase } from '../use-cases/create-user.js'
+import { badRequest, created, serverError } from './helpers.js'
+
+export class CreateUserController {
+    async execute(httpRequest) {
+        try {
+            const params = httpRequest.body
+            const requiredFields = [
+                'first_name',
+                'last_name',
+                'email',
+                'password',
+            ]
+
+            for (const field of requiredFields) {
+                if (!params[field] || !params[field].trim().length) {
+                    return badRequest({ message: `Missing param: ${field}` })
+                }
+            }
+
+            const isValidPassword = params.password.length > 5
+            if (!isValidPassword) {
+                return badRequest({
+                    message: 'Password must be at least 6 characters',
+                })
+            }
+
+            const isValidEmail = validator.isEmail(params.email)
+            if (!isValidEmail) {
+                return badRequest({
+                    message: 'Invalid e-mail. Please provide a valid one',
+                })
+            }
+
+            const createUserUseCase = new CreateUserUseCase()
+            const createdUser = await createUserUseCase.execute({ ...params })
+
+            return created(createdUser)
+        } catch (error) {
+            console.error(error)
+            return serverError()
+        }
+    }
+}
