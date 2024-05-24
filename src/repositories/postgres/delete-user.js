1b1344f8fe1b441fe2099f9f6eaab3b703c8c7e4
JhonatanGAlves <jhonatangalves96@gmail.com>
2024-05-23 22:33:11 -0300

    feat: ✨ creating DeleteUserRepository class

---
+++ src/repositories/postgres/delete-user.js
@@ -0,0 +1,11 @@
+import { PostgresHelper } from '../../db/postgres/helper.js'
+
+export class PostgresDeleteUserRepository {
+    async execute(userId) {
+        const userDeletedById = await PostgresHelper.query(
+            'DELETE FROM users WHERE id = $1',
+            [userId],
+        )
+        return userDeletedById
+    }
+}
