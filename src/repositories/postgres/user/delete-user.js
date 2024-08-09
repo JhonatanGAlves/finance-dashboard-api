ee63473326e44aaabddf0089d175ce6d44da17d2
JhonatanGAlves <jhonatangalves96@gmail.com>
2024-08-09 14:53:39 -0300

    feat: ✨ using Prisma in DeleteUserRepository

--- src/repositories/postgres/user/delete-user.js
+++ src/repositories/postgres/user/delete-user.js
@@ -1,15 +1,11 @@
-import { PostgresHelper } from '../../../db/postgres/helper.js'
+import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresDeleteUserRepository {
     async execute(userId) {
-        const userDeletedById = await PostgresHelper.query(
-            `
-            DELETE FROM users
-            WHERE id = $1
-            RETURNING *
-            `,
-            [userId],
-        )
-        return userDeletedById[0]
+        return await prisma.user.delete({
+            where: {
+                id: userId,
+            },
+        })
     }
 }
